7ab52e04e460b6f3799295590a5e8a8df89bf789
Barys Barysau <barysaubarys@gmail.com>
2023-06-30 13:43:23 +0400

    <feat>http add

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Hi");
+import * as http from "node:http";
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write("Hello World!");
+    res.end();
+  })
+  .listen(3000);
