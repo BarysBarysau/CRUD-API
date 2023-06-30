698a762419509716dab7056772b07423fce407c2
Barys Barysau <barysaubarys@gmail.com>
2023-06-30 21:57:37 +0400

    <fix>fix UserService

--- src/service/UserService.js
+++ src/service/UserService.js
@@ -1,19 +1,25 @@
 import { v4 as uuidv4 } from "uuid";
+let array = [];
 class Users {
-  users = {};
   constructor(username, age, hobbies) {
-    const id = uuidv4();
-    this.users[id] = {};
-    this.users[id].username = username;
-    this.users[id].age = age;
-    this.users[id].hobbies = hobbies;
+    this.id = uuidv4();
+    this.username = username;
+    this.age = age;
+    this.hobbies = hobbies;
   }
 
-  getUsers() {
-    return this.users;
+  addUser() {
+    array.push(this);
+  }
+
+  static getUsers() {
+    return array;
   }
 }
 
 const user = new Users("Yan", 4, ["football", "ice-cream", "puri"]);
 const user2 = new Users("Vi", 8, ["football", "ice-cream", "puri"]);
-console.log(user2.getUsers());
+user.addUser();
+user2.addUser();
+
+console.log(Users.getUsers());
